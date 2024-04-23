da0a9ab97a61d62fab8b96d8b7837ced8085fd5d
pro-khar <prakharvermamanu.veed@gmail.com>
2024-04-23 20:36:22 +0530

    Deployment Successfull

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/powernotes",
+  base: "/notes_shadcn",
   plugins: [react()],
   resolve: {
     alias: {
